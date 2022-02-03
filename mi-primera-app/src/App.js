a6c4aba9764817f352d30cd2dd50e48d81942109
kyanesdev <kyanesdev@gmail.com>
2022-02-03 15:50:49 -0300

    componentes

--- mi-primera-app/src/App.js
+++ mi-primera-app/src/App.js
@@ -1,38 +1,80 @@
-import logo from './logo.svg';
-//import './App.css';
-import './main.css';
+/* import Button from './Button.js'
+import './Button.css'
 
-const estilo2 = {
-  boxShadow: '0 5px 3px rgba(0, 0, 0, 0.5)'
-}
 
-const estilo = ({bg = '#222'})=> ({
-  backgroundColor: bg,
-  color: '#fff',
-  padding: '2rem',
-  margin: '10px 15px'
-})
+const arr = [
+  'porki feliz',
+  'porki triste',
+  'porki emocionado'
+]
 
-const Li = ({children})=>{
+const App = ()=>{
 
+  const miVariable = false
+
+  if(miVariable){
+
+    return <p>Mi variable dio true!</p>
+
+  }
 
   return (
-    <li className='clase-li'>{children}</li>
+    <div>
+      <h1 onClick={(e)=>console.log('click' , e)}>Hola Mundo</h1>
+      {arr.map(el => <p key={el}>{el}</p> )}
+      
+      <Button onClick={()=>console.log('clickeado')}>
+        Enviar
+      </Button>
+    </div>
   )
+
+} */
+
+//Componente basado en clases
+
+import {Component} from 'react'
+
+class Button extends Component {
+    render(){
+
+      console.log('ejecutando metodo render de button')
+      return (
+        <button>Enviar</button>
+      )
+
+    }
+}
+class App extends Component {
+
+    //Esto es el estado de un componente
+    //puede contener propiedades y tiene que estar escrito
+    //si o si en ingles "state" ya que estado no lo tomaria
+    state = {
+      valor: 3
+    }
+
+    //Es como los componentes funcionales que retornan contenido
+    //en JSX
+    render(){
+
+      console.log(this.state);
+      
+      return (
+        <div>
+          <p>Hola Mundo</p>
+          <Button></Button>
+          //el componente se actualiza cuando llamamos a setState
+          <button className={`${this.state.valor}`} onClick={()=> this.setState({valor:2})}>
+            Enviarselo a App
+          </button>
+        </div>
+      )
+    }
+
+
 }
 
 
-//Un componente de react puede ser declarado como funcion
-//o tambien puede ser declarado como una constante con la arrow function
-function App() {
 
-  return (
-    <ul className="clase-css">
-
-      <Li estado="sadly">valor de li</Li>
-
-    </ul> 
-  );
-}
-
-export default App;
+export default App
---
+++ mi-primera-app/src/Button.js
@@ -0,0 +1,7 @@
+const Button = (props)=>{
+    return(
+        <button {...props} className='btn'></button>
+    )
+}
+
+export default Button
